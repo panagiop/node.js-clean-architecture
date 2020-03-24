a32c836c67cbd5ba80a0b9c305a173e36dd5a6d6
panos <panagiotis8panagiotopoulos@gmail.com>
2020-03-24 02:41:13 +0200

    change in API endpoints

--- frameworks/webserver/routes/index.js
+++ frameworks/webserver/routes/index.js
@@ -2,6 +2,6 @@ import postRouter from './post';
 import userRouter from './user';
 
 export default function Routes(app, express) {
-  app.use('/api/post', postRouter(express));
-  app.use('/api/user', userRouter(express));
+  app.use('/api/v1/posts', postRouter(express));
+  app.use('/api/v1/users', userRouter(express));
 }
