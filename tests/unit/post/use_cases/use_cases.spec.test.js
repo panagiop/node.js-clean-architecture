e3ed2ff8bdfa04e0446fa818b305b7db9a14d7ce
panos <panagiotis8panagiotopoulos@gmail.com>
2021-02-16 17:28:06 +0200

    fix unit tests for post

--- tests/unit/post/use_cases/use_cases.spec.test.js
+++ tests/unit/post/use_cases/use_cases.spec.test.js
@@ -4,17 +4,17 @@ import sinon from 'sinon';
 import faker from 'faker';
 const expect = chai.expect;
 
-import Post from '../../../../src/entities/post';
-import AddPost from '../../../../application/use_cases/post/add';
-import FindAll from '../../../../application/use_cases/post/findAll';
-import FindById from '../../../../application/use_cases/post/findById';
-import PostDbRepository from '../../../../application/repositories/postDbRepository';
+import post from '../../../../src/entities/post';
+import addPost from '../../../../application/use_cases/post/add';
+import findAll from '../../../../application/use_cases/post/findAll';
+import findById from '../../../../application/use_cases/post/findById';
+import postDbRepository from '../../../../application/repositories/postDbRepository';
 
 let dbRepository = null;
 
 describe('Use cases', () => {
   beforeEach(() => {
-    dbRepository = PostDbRepository();
+    dbRepository = postDbRepository();
   });
 
   describe('Fetch a specific post', () => {
@@ -26,24 +26,24 @@ describe('Use cases', () => {
         isPublished: false,
         userId: faker.random.uuid()
       };
-      const correspondingPost = Post(
-        stubPost.title,
-        stubPost.description,
-        stubPost.createdAt,
-        stubPost.isPublished,
-        stubPost.userId,
-        dbRepository
-      );
+      const correspondingPost = post({
+        title: stubPost.title,
+        description: stubPost.description,
+        createdAt: stubPost.createdAt,
+        isPublished: stubPost.isPublished,
+        userId: stubPost.userId,
+        postRepository: dbRepository
+      });
       const stubRepositoryFindById = sinon
         .stub(dbRepository, 'findById')
         .returns(correspondingPost);
-      const post = FindById('5fb1b12a6ac3e23493ac82e4', dbRepository);
+      const fetchedPost = findById('5fb1b12a6ac3e23493ac82e4', dbRepository);
       expect(stubRepositoryFindById.calledOnce).to.be.true;
       sinon.assert.calledWith(
         stubRepositoryFindById,
         '5fb1b12a6ac3e23493ac82e4'
       );
-      expect(post).to.eql(correspondingPost);
+      expect(fetchedPost).to.eql(correspondingPost);
     });
   });
 
@@ -52,7 +52,7 @@ describe('Use cases', () => {
       const stubRepositoryFetchAll = sinon
         .stub(dbRepository, 'findAll')
         .returns(['post1', 'post2']);
-      const posts = FindAll(dbRepository);
+      const posts = findAll(dbRepository);
       expect(stubRepositoryFetchAll.calledOnce).to.be.true;
       expect(posts).to.eql(['post1', 'post2']);
     });
@@ -67,24 +67,24 @@ describe('Use cases', () => {
         isPublished: false,
         userId: faker.random.uuid()
       };
-      const pesristedPost = Post(
-        stubValue.title,
-        stubValue.description,
-        stubValue.createdAt,
-        stubValue.isPublished,
-        stubValue.userId
-      );
+      const pesristedPost = post({
+        title: stubValue.title,
+        description: stubValue.description,
+        createdAt: stubValue.createdAt,
+        isPublished: stubValue.isPublished,
+        userId: stubValue.userId
+      });
       const stubRepositoryAdd = sinon
         .stub(dbRepository, 'add')
         .returns(pesristedPost);
-      const newPost = AddPost(
-        stubValue.title,
-        stubValue.description,
-        stubValue.createdAt,
-        stubValue.isPublished,
-        stubValue.userId,
-        dbRepository
-      );
+      const newPost = addPost({
+        title: stubValue.title,
+        description: stubValue.description,
+        createdAt: stubValue.createdAt,
+        isPublished: stubValue.isPublished,
+        userId: stubValue.userId,
+        postRepository: dbRepository
+      });
       expect(stubRepositoryAdd.calledOnce).to.be.true;
       expect(newPost.getTitle()).equals(stubValue.title);
       expect(newPost.getDescription()).equals(stubValue.description);
