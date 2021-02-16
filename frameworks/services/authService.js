f360233ee09d89c43661b8890ea0b370b836823e
panos <panagiotis8panagiotopoulos@gmail.com>
2021-02-16 13:21:05 +0200

    rename auth service

---
+++ frameworks/services/authService.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+import config from '../../config/config';
+
+export default function authServiceImpl() {
+  const encryptPassword = (password) => {
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
+  };
+
+  const compare = (password, hashedPassword) =>
+    bcrypt.compareSync(password, hashedPassword);
+
+  const verify = (token) => jwt.verify(token, config.jwtSecret);
+
+  const generateToken = (payload) => {
+    return jwt.sign(payload, config.jwtSecret, {
+      expiresIn: 360000
+    });
+  };
+
+  return {
+    encryptPassword,
+    compare,
+    verify,
+    generateToken
+  };
+}
