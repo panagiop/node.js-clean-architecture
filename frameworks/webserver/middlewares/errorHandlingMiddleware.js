1c5001f3a123b396a39b9f5b280412e1f2286eca
panos <panagiotis8panagiotopoulos@gmail.com>
2021-02-20 02:01:26 +0200

    uglier / prettier ...

--- frameworks/webserver/middlewares/errorHandlingMiddleware.js
+++ frameworks/webserver/middlewares/errorHandlingMiddleware.js
@@ -3,11 +3,9 @@
 export default function errorHandlingMiddlware(err, req, res, next) {
   err.statusCode = err.statusCode || 404;
   return err.customMessage || err.message
-    ? res
-        .status(err.statusCode)
-        .json({
-          status: err.statusCode,
-          message: err.customMessage || err.message
-        })
+    ? res.status(err.statusCode).json({
+        status: err.statusCode,
+        message: err.customMessage || err.message
+      })
     : res.status(err.statusCode).json({ status: err.statusCode, message: err });
 }
